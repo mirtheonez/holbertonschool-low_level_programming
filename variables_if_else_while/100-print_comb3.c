b80d6b9966676e8a627c9f72f0708d9cf20fd96b
nourbenhazem <claudiabenhazem@gmail.com>
2023-03-15 14:04:22 +0100

    100

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i1, i2;
+for (i1 = 0; i1 < 9; i1++)
+{
+for (i2 = i1 + 1; i2 < 10; i2++)
+{
+putchar((i1 % 10) + '0');
+putchar((i2 % 10) + '0');
+if (i1 == 8 && i2 == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
