daaaacc16bb4754e40f75eb238d7477746a7bb60
nourbenhazem <claudiabenhazem@gmail.com>
2023-04-26 15:19:32 +0100

    0

--- static_librairy/0-isupper.c
+++ static_librairy/0-isupper.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
-*_isupper - checks for uppercase character.
-*@c: input to be checked.
-*Return: 1 if uppercase and 0 otherwise.
+* _isupper - check if num=[0-9]
+* @c: int
+* Return: Always 0 (Success)
 */
+
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
+if (c > 64 && c <= 90)
 return (1);
 else
 return (0);
