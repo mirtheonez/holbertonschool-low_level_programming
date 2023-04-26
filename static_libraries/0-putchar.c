4d59df0f02ad187dc218caac5d1aa3a5a6cc4f8e
nourbenhazem <claudiabenhazem@gmail.com>
2023-04-26 15:39:57 +0100

    putchar

--- static_libraries/0-putchar.c
+++ static_libraries/0-putchar.c
@@ -1,13 +1,11 @@
-#include <unistd.h>
-
+#include <stdio.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+*main -Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
 {
-	return (write(1, &c, 1));
+puts("_putchar");
+return (0);
 }
