b2125a40c44002056a0b95ba03621f3d6b71bc86
nourbenhazem <claudiabenhazem@gmail.com>
2023-05-06 21:18:28 +0100

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,15 +12,22 @@ char *_strdup(char *str)
 {
 	char *d;
 	int i, l = 0;
+
 	if (str == NULL)
 		return (NULL);
+
 	for (i = 0; str[i]; i++)
 		l++;
+
 	d = malloc(sizeof(char) * (l + 1));
+
 	if (d == NULL)
 		return (NULL);
+
 	for (i = 0; str[i]; i++)
 		d[i] = str[i];
+
 	d[l] = '\0';
+
 	return (d);
 }
